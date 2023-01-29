80124d0b6710374dd87dce6d3c53db84ffa3733b
JongHoonSon <roka1684@gmail.com>
2023-01-30 02:01:28 +0900

    Deploy: PORT 5000으로 고정

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import app from "./app";
 
 createConnection();
 
-const port = process.env.PORT || 5000;
+const port = 5000;
 
 app.listen(port, () => {
   console.log(`Now listening on port ${port}`);
